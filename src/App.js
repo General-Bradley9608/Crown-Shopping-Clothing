e5e6be9b58ef2b1a9b30d8bb382e1e18aa00a8e9
General-Bradley9608 <bradleyhelphenstine9608@gmail.com>
2020-04-10 12:01:07 +0300

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ class App extends React.Component {
                 id: snapShot.id,
                 ...snapShot.data(),
               },
-            },
-            () => {
-              console.log(this.state);
             }
+            // () => {
+            //   console.log(this.state);
+            // }
           );
         });
       } else {
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -22,10 +22,17 @@ class SignIn extends React.Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   render() {
