3025774bf36004eefc2ef9c9bf36f290822efe86
General-Bradley9608 <bradleyhelphenstine9608@gmail.com>
2020-04-12 23:13:43 +0300

    implementing redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
