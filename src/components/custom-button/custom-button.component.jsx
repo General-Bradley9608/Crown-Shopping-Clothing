d13b6e10751ec01fbf8522c6e34adb2f9f28a198
General-Bradley9608 <bradleyhelphenstine9608@gmail.com>
2020-04-17 17:31:09 +0300

    converted custom-button and header and homepage to styled-components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+// import './custom-button.styles.scss';
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? 'inverted' : ''} ${
-      isGoogleSignIn ? 'google-sign-in' : ''
-    } custom-button`}
-    {...otherProps}
-  >
+import { CustomButtonContainer } from './custom-button.styles';
+
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer className={'custom-button'} {...props}>
     {children}
-  </button>
+  </CustomButtonContainer>
 );
 
 export default CustomButton;
