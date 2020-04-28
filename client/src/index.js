7ae23c8377d352183d856eb6b7ed6ac2d700451a
General-Bradley9608 <bradleyhelphenstine9608@gmail.com>
2020-04-28 11:30:59 +0300

    updated the code to be pwa compliant, added serviceworker, https, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
 
+import * as serviceWorker from './serviceWorker';
+
 import './index.css';
 import App from './App';
 
@@ -20,3 +22,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
@@ -11,7 +11,18 @@ const HomePage = () => {
 
   return (
     <HomePageContainer>
-      <Directory />
+      <Profiler
+        id='Directory'
+        onRender={(id, phase, actualDuration) => {
+          // console.log({
+          //   id,
+          //   phase,
+          //   actualDuration,
+          // });
+        }}
+      >
+        <Directory />
+      </Profiler>
     </HomePageContainer>
   );
 };
